c836c2f20fc511c8807258c3e2b5d62301f5092b
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2020-04-19 17:07:21 +0200

    Moved model related methods to separate class

---
+++ ModelDrawing.cpp
@@ -0,0 +1,88 @@
+#include <windows.h>
+#include "glut.h"
+#include "model3DS.h"
+#include <time.h>
+#include <direct.h>
+#include <GL/glu.h>
+
+class ModelDrawing
+{
+public:
+	struct model_w_skladzie {
+		char* filename;
+		model3DS* model;
+		struct model_w_skladzie* wsk;
+	};
+
+	struct model_w_skladzie* sklad_modeli = NULL;
+
+	void dodajModel(model3DS* _model, char* file_name)
+	{
+		struct model_w_skladzie* tmp;
+		tmp = (struct model_w_skladzie*) malloc(sizeof(struct model_w_skladzie));
+		tmp->filename = (char*)malloc(strlen(file_name) + 1);
+		strcpy(tmp->filename, file_name);
+		tmp->model = _model;
+		tmp->wsk = sklad_modeli;
+		sklad_modeli = tmp;
+	}
+
+	model3DS* pobierzModel(char* file_name)
+	{
+		struct model_w_skladzie* sklad_tmp = sklad_modeli;
+		while (sklad_tmp) {
+			if (!_stricmp(sklad_tmp->filename, file_name)) return sklad_tmp->model;
+			char file_name_full[_MAX_PATH];
+			strcpy(file_name_full, file_name);
+			strcat(file_name_full, ".3ds");
+			if (!_stricmp(sklad_tmp->filename, file_name_full)) return sklad_tmp->model;
+
+			sklad_tmp = sklad_tmp->wsk;
+		}
+		return NULL;
+	}
+
+	void rysujModel(char* file_name, int tex_num = -1)
+	{
+		model3DS* model_tmp;
+		if (model_tmp = pobierzModel(file_name))
+			if (tex_num == -1)
+				model_tmp->draw();
+			else
+				model_tmp->draw(tex_num, false);
+
+	}
+
+	void aktywujSpecjalneRenderowanieModelu(char* file_name, int spec_id = 0)
+	{
+		model3DS* model_tmp;
+		if (model_tmp = pobierzModel(file_name))
+			model_tmp->setSpecialTransform(spec_id);
+	}
+
+	void ladujModele()
+	{
+		WIN32_FIND_DATA* fd;
+		HANDLE fh;
+		model3DS* model_tmp;
+		char directory[_MAX_PATH];
+		if (_getcwd(directory, _MAX_PATH) == NULL) return;
+		strcat(directory, "\\data\\*.3ds");
+
+		fd = (WIN32_FIND_DATA*)malloc(sizeof(WIN32_FIND_DATA));
+		fh = FindFirstFile((LPCSTR)directory, fd);
+		if (fh != INVALID_HANDLE_VALUE)
+			do {
+				if (fd->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {	// katalogi ignorujemy
+					if (FindNextFile(fh, fd)) continue; else break;
+				}
+				// ladowanie obiektu i dodanie do kontenera
+				char filename[_MAX_PATH];
+				strcpy(filename, "data\\");
+				strcat(filename, fd->cFileName);
+				model_tmp = new model3DS(filename, 1, false);
+				dodajModel(model_tmp, fd->cFileName);
+				printf("[3DS] Model '%s' stored\n", fd->cFileName);
+			} while (FindNextFile(fh, fd));
+	}
+};
--- rysowanie.cpp
+++ rysowanie.cpp
@@ -29,33 +29,33 @@ const int CROSSROAD_LENGTH = 7;
 	//glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
 	glPushMatrix();
 		glTranslatef(0,-1,0);
-		rysujModel("desert_terrain"); // malowanie pod³o¿a
-		rysujModel("niebo"); // malowanie nieba
+		modelHelper.rysujModel("desert_terrain"); // malowanie pod³o¿a
+		modelHelper.rysujModel("niebo"); // malowanie nieba
 	glPopMatrix();
 	
 
 	// MODELE 3ds:
 	// Modele 3ds znajdujace sie w katalogu /data s¹ autoamtycznie ladowane i rejestrowane pod nazwami zbieznymi z nazwami plikow
-	// Aby narysowaæ model nalezy wywo³aæ funkcjê: rysujModel ("nazwa_modelu");
+	// Aby narysowaæ model nalezy wywo³aæ funkcjê: modelHelper.rysujModel ("nazwa_modelu");
 	// Nazwa_modelu mo¿e byæ podana literemi du¿ymi lub ma³ymi, z rozszerzeniem pliku lub bez.
 		
 	// przyklad:	
 
 	glPushMatrix();
 		glTranslatef(-20, 0.3, -50);
-		rysujModel("wiatrak");
+		modelHelper.rysujModel("wiatrak");
 	glPopMatrix();
 
 	glPushMatrix();
 	glTranslatef(-5, 0.3, 45);
-	rysujModel("Semaphore");
+	modelHelper.rysujModel("Semaphore");
 	glPopMatrix();
 
 	// pallet
 	glPushMatrix();
 	glTranslatef(0, 0.3, -4.2);
 	glScalef(1, 0.65, 1.1);
-	rysujModel("shelf");
+	modelHelper.rysujModel("shelf");
 	glPopMatrix();
 	
 	// forklift1 animation
@@ -76,16 +76,16 @@ const int CROSSROAD_LENGTH = 7;
 
 		glRotatef(-90,0,1,0);
 
-		rysujModel("forklift2");
+		modelHelper.rysujModel("forklift2");
 
 		glTranslatef(0, topPartPosition, 0);
-		rysujModel("topPart");
+		modelHelper.rysujModel("topPart");
 
 		glTranslatef(0, forkPosition, 0);
-		rysujModel("fork");
+		modelHelper.rysujModel("fork");
 
 		glTranslatef(-3.8, 10.1, 1.1);
-		rysujModel("pallet");
+		modelHelper.rysujModel("pallet");
 
 	glPopMatrix();
 
@@ -94,12 +94,12 @@ const int CROSSROAD_LENGTH = 7;
 	// slupy
 	glPushMatrix();
 		glTranslatef(-67, 0.3, -57);
-		rysujModel("linia_slup");
+		modelHelper.rysujModel("linia_slup");
 
 		for (int i = 0; i < 9; i++)
 		{
 			glTranslatef(0, 0, 10.5);
-			rysujModel("linia");
+			modelHelper.rysujModel("linia");
 		}
 		
 	glPopMatrix();
@@ -107,25 +107,25 @@ const int CROSSROAD_LENGTH = 7;
 	// tory
 	glPushMatrix();
 		glTranslatef(40, 0.3, -57);
-		rysujModel("tor");
+		modelHelper.rysujModel("tor");
 
 		for (int i = 0; i < 6; i++)
 		{
 			glTranslatef(0, 0, 13.5);
-			rysujModel("tor");
+			modelHelper.rysujModel("tor");
 		}
 
-		rysujModel("tor_zakret");
+		modelHelper.rysujModel("tor_zakret");
 
 		glRotatef(-30, 0, 1, 0);
 		glTranslatef(3.42, 0, 14);
 
-		rysujModel("tor_zakret");
+		modelHelper.rysujModel("tor_zakret");
 
 		glRotatef(-30, 0, 1, 0);
 		glTranslatef(3.42, 0, 14);
 
-		rysujModel("tor_zakret");
+		modelHelper.rysujModel("tor_zakret");
 
 		glRotatef(-30, 0, 1, 0);
 		glTranslatef(3.42, 0, 14.2);
@@ -133,7 +133,7 @@ const int CROSSROAD_LENGTH = 7;
 		for (int i = 0; i < 10; i++)
 		{
 			glTranslatef(0, 0, 13.5);
-			rysujModel("tor");
+			modelHelper.rysujModel("tor");
 		}
 		
 	glPopMatrix();
@@ -141,72 +141,72 @@ const int CROSSROAD_LENGTH = 7;
 	// droga
 	glPushMatrix();
 		glTranslatef(0, 0.3, -57);
-		rysujModel("droga_prosta");			
+		modelHelper.rysujModel("droga_prosta");			
 
 		glTranslatef(0, 0, 7);
-		rysujModel("droga_przejscie");
+		modelHelper.rysujModel("droga_przejscie");
 		
 		glTranslatef(0, 0, ROAD_LENGTH);
-		rysujModel("droga_prosta");
+		modelHelper.rysujModel("droga_prosta");
 
 		glTranslatef(0, 0, CROSSROAD_LENGTH);
-		rysujModel("droga_skrzyzowanie");
+		modelHelper.rysujModel("droga_skrzyzowanie");
 
 		glPushMatrix();
 			glTranslatef(0, 0, ROAD_LENGTH + CROSSROAD_LENGTH);
-			rysujModel("droga_prosta");
+			modelHelper.rysujModel("droga_prosta");
 
 			glTranslatef(0, 0, ROAD_LENGTH);
-			rysujModel("droga_prosta");
+			modelHelper.rysujModel("droga_prosta");
 
 			glTranslatef(0, 0, ROAD_LENGTH);
-			rysujModel("droga_prosta");
+			modelHelper.rysujModel("droga_prosta");
 
 			glTranslatef(0, 0, CROSSROAD_LENGTH);
 			glRotatef(90, 0, 1, 0);
-			rysujModel("droga_zakret");
+			modelHelper.rysujModel("droga_zakret");
 
 			glTranslatef(0, 0, 10);
 			glRotatef(90, 0, 1, 0);
-			rysujModel("droga_koniec");
+			modelHelper.rysujModel("droga_koniec");
 		glPopMatrix();
 
 		// back to crossroad
 		glRotatef(90, 0, 1, 0);
 
 		glTranslatef(0, 0, -CROSSROAD_LENGTH);
-		rysujModel("droga_prosta");
+		modelHelper.rysujModel("droga_prosta");
 
 		glTranslatef(0, 0, -ROAD_LENGTH);
-		rysujModel("droga_prosta");
+		modelHelper.rysujModel("droga_prosta");
 
 		glRotatef(-270, 0, 1, 0);
 		glTranslatef(ROAD_LENGTH + 7, 0, 0);
-		rysujModel("droga_skrzyzowanie");
+		modelHelper.rysujModel("droga_skrzyzowanie");
 
 		glPushMatrix();
 			glTranslatef(0, 0, -7);
-			rysujModel("droga_prosta");
+			modelHelper.rysujModel("droga_prosta");
 
 			glTranslatef(0, 0, -ROAD_LENGTH);
-			rysujModel("droga_przejscie");
+			modelHelper.rysujModel("droga_przejscie");
 
 			glTranslatef(0, 0, -7);
-			rysujModel("droga_prosta");
+			modelHelper.rysujModel("droga_prosta");
 
 
 			glTranslatef(0, 0, -ROAD_LENGTH);
 			glRotatef(-90, 0, 1, 0);
 
-			rysujModel("droga_koniec");
+			modelHelper.rysujModel("droga_koniec");
 		glPopMatrix();
 
 		// back to crossroad
 		glTranslatef(0, 0, ROAD_LENGTH + CROSSROAD_LENGTH);
-		rysujModel("droga_prosta");
+		modelHelper.rysujModel("droga_prosta");
 
 		glTranslatef(0, 0, ROAD_LENGTH);
-		rysujModel("droga_prosta");
+		modelHelper.rysujModel("droga_prosta");
 	glPopMatrix();
 
 	
